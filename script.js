6a3126c519edd265ef954e2a17bb4e7c520e48f3
Vipul singh <101138758+VipulSingh78@users.noreply.github.com>
2023-10-29 22:42:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+var $magellan = 'magellan-expedition';
+  $(document).foundation({
+    "magellan-expedition": {
+      threshold: 0, 
+      destination_threshold: 12, 
+      throttle_delay: 50, 
+      fixed_top: 0, 
+      offset_by_height: true 
+    }
+  });
+  $(document).foundation();
